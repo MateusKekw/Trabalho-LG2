47c7987100dc60c88a924fb8e0653470fc484692
Mateus <126785833+MateusKekw@users.noreply.github.com>
2023-10-22 10:51:19 -0300

    Update Bolsa.java
    adicionei funções de banco de dados

--- TrabaioLG2/src/main/java/entity/Bolsa.java
+++ TrabaioLG2/src/main/java/entity/Bolsa.java
@@ -1,4 +1,22 @@
 package entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table
 public class Bolsa {
+    @Column
+    private String nome = "B3";
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    //só para não dar erro no @Table
+    @Column
+    private List<Corretora> empresas_cadastradas;
+    @Column
+    private List<Investidor> investidores;
+    @Column
+    private List<Ativos> acoes_circulando;
 }
