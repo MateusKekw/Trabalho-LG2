8e36fee5b788907096dc6bf91a1824278de0b101
Mateus <126785833+MateusKekw@users.noreply.github.com>
2023-10-22 10:52:04 -0300

    Update Corretora.java
    adicionei funções de banco de dados do hibernate

--- TrabaioLG2/src/main/java/entity/Corretora.java
+++ TrabaioLG2/src/main/java/entity/Corretora.java
@@ -1,4 +1,23 @@
 package entity;
 
+import jakarta.persistence.*;
+import org.hibernate.boot.model.naming.Identifier;
+
+import java.util.List;
+
+@Entity
+@Table
 public class Corretora {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id_Corretora;
+    @Column
+    private String nome;
+    @Column
+    private char Sigla;
+    @Column
+    private List<Ativos> movimentacoes;
+    @Column
+    private Integer classificacao;
+
 }
